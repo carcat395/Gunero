3531a240bb4b5b8d4126081f63495fa694fbb472
carcat395 <71756224+carcat395@users.noreply.github.com>
2020-11-13 21:44:21 +0700

    aa

---
+++ Gunero/Assets/Scripts/EnemyBuilderEditor.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(EnemyBuilderEditor))]
+[CanEditMultipleObjects]
+public class EnemyBuilderEditor : Editor
+{
+    
+}
